813162735b36b59d74ac44b27921a629be27e665
Stoyan Stoyanov <chaneto800ss@gmail.com>
2022-08-12 15:46:44 +0300

    update

--- src/main/java/com/example/game/web/UserController.java
+++ src/main/java/com/example/game/web/UserController.java
@@ -53,5 +53,4 @@ public class UserController {
   public ResponseEntity<List<UserBestGameResource>> getAllUserWithBestGame() {
     return new ResponseEntity<>(this.userService.getAllUserWithBestGame(), HttpStatus.OK);
   }
-  int a = 5
 }
